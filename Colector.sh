d3b96ce2398080292c105a175eca5c75a368fa4f
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-01-16 16:11:16 +0100

    Change number of threads in Colector.sh script

--- Colector.sh
+++ Colector.sh
@@ -8,7 +8,7 @@ for (( i=1; i<=5; i++))
 do
 
 	times=()
-	for (( k=1; k<=16; k++))
+	for (( k=1; k<=200; k++))
 	do
 		times=("${times[@]}" "$(python3 PDP11.py $k),")
 		echo "En la $i interación de 5 con $k núcleos tardó: ${times[-1]::-1} nanosegundos."

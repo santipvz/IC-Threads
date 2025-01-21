7b3f643e5415b5138a63cb8fd24010911c9b259d
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-01-21 17:00:44 +0100

    Added external files

--- Colector.sh
+++ Colector.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 rm -fr times.txt
 clear
-CPU=$(cat /proc/cpuinfo  | grep "name"| uniq)
+CPU=$(cat /proc/cpuinfo | grep "name" | uniq)
 CPU="${CPU:13}"
-echo "$CPU" >> times.txt
-for (( i=1; i<=5; i++))
-do
+echo "$CPU" >>times.txt
 
+iterations=$(cat iterations.txt)
+threads=$(cat threads.txt)
+
+for ((i = 1; i <= iterations; i++)); do
 	times=()
-	for (( k=1; k<=200; k++))
-	do
+
+	for ((k = 1; k <= threads; k++)); do
 		times=("${times[@]}" "$(python3 PDP11.py $k),")
 		echo "En la $i interación de 5 con $k núcleos tardó: ${times[-1]::-1} nanosegundos."
 	done
 
-
 	len="$(expr ${#times[@]} - 1)"
 
-
 	times=("${times[@]::$len}" "${times[-1]::-1}")
-	echo "${times[@]}" >> times.txt
+	echo "${times[@]}" >>times.txt
 
 done

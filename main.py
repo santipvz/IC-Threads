e415010c9e1d41d2a21fb73b52282e5eb5b0185e
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-01-21 16:46:35 +0100

    Added csv with data

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import subprocess
 import cpuinfo
 import os
+import pandas as pd
 
 
 if __name__ == "__main__":
+
     cpuName = cpuinfo.get_cpu_info()["brand_raw"]
     nCores = os.cpu_count()
+
+    # We check that data.csv exists
+    if os.path.exists("data.csv"):
+
+        # We read the data
+        data = pd.read_csv("data.csv")
+
+    else:
+        data = pd.DataFrame(columns=["CPU", "Cores", "Threads", "Iteration", "Time"])
+
+    data = data.astype(
+        {
+            "CPU": "string",
+            "Cores": "int",
+            "Threads": "int",
+            "Iteration": "int",
+            "Time": "float",
+        }
+    )
+
+    # Set the combination of columns as a multi-index
+    data.set_index(["CPU", "Cores", "Threads", "Iteration"], inplace=True)
 
     subprocess.run(["bash", "Colector.sh"])
 
@@ -16,3 +40,20 @@ if __name__ == "__main__":
     # The first line needs to be the cpu name
     if info[0] != cpuName:
         raise Exception(f"CPU name mismatch: {info[0]} != {cpuName}")
+
+    newInfo = []
+
+    # Each line is an iteration
+    for i, line in enumerate(info[1:]):
+        # Each number is with a different amount of threads
+        for j, time in enumerate(line.split(",")):
+            newInfo.append({"Threads": j + 1, "Iteration": i + 1, "Time": float(time)})
+
+    # We add the new data to the dataframe
+    # We do it in a loop in order to not occupy to much memory
+    for row in newInfo:
+        rowIndex = (cpuName, nCores, row["Threads"], row["Iteration"])
+        data.loc[rowIndex] = row["Time"]
+
+    # We save the data
+    data.to_csv("data.csv", index=True)
